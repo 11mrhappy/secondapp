9c97384ca86bd75a515b9aa8ecea980c89673dcf
11mrhappy <nekomamiremy@gmail.com>
2021-02-17 15:05:34 +0900

    add shared_preferences

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
